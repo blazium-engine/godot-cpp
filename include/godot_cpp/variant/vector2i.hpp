59037da3dc73913aa67a4eaa346b63a478a2efd1
Mounir Tohami <53877170+WhalesState@users.noreply.github.com>
2024-11-26 16:37:35 +0200

    update cpp api

--- include/godot_cpp/variant/vector2i.hpp
+++ include/godot_cpp/variant/vector2i.hpp
@@ -144,6 +144,21 @@ struct _NO_DISCARD_ Vector2i {
 		x = p_x;
 		y = p_y;
 	}
+	inline explicit Vector2i(int32_t p_v) {
+		x = p_v;
+		y = p_v;
+	}
+	inline Vector2i(const Vector2i &p_v) noexcept {
+		x = p_v.x;
+		y = p_v.y;
+	}
+	inline Vector2i &operator=(const Vector2i &p_v) {
+		if (this != &p_v) {
+			x = p_v.x;
+			y = p_v.y;
+		}
+		return *this;
+	}
 };
 
 // Multiplication operators required to workaround issues with LLVM using implicit conversion.
